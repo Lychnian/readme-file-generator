e3673a2545b0999392cbd6b8957f4ce0bc6e3ce7
unknown <helenvcolon@gmail.com>
2023-11-29 18:17:24 -0500

    Added license badge functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,19 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Function to render license badge
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+      return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  } else {
+      // Return MIT badge as default when license is 'None' or not specified
+      return `![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)`;
+  }
+}
+// Function to render license link
 function renderLicenseLink(license) {}
+// Return MIT link as default when license is 'None' or not specified
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Function to render license section of README
 function renderLicenseSection(license) {}
+// Return MIT section as default when license is 'None' or not specified
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
